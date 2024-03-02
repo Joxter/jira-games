d46ecb7d7b356b90d0a4577ff01584ffcf91646d
Joxter <joxterrr@gmail.com>
2024-03-02 15:28:29 +0100

    some preparations

---
+++ src/story/StoryPage.tsx
@@ -0,0 +1,15 @@
+import css from "./StoryPage.module.css";
+
+export function StoryPage() {
+  return (
+    <div class={css.root}>
+      <div>header</div>
+      <div>left</div>
+      <div>
+        <h1>Stpry page</h1>
+      </div>
+      <div>right</div>
+      <div>footer</div>
+    </div>
+  );
+}
