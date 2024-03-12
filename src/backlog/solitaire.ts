20b7c9eb2bec08a008dd99a7b61c78213d9c2f0e
Joxter <joxterrr@gmail.com>
2024-03-12 19:53:18 +0100

    update solitaire

--- src/backlog/solitaire.ts
+++ src/backlog/solitaire.ts
@@ -1,20 +1,22 @@
 // Solitaire game
 
 const CardSuits = {
-  HEARTS: { name: "hearts", char: "♥", n: 0, color: "red" },
-  DIAMONDS: { name: "diamonds", char: "♦", n: 1, color: "red" },
-  CLUBS: { name: "clubs", char: "♣", n: 2, color: "black" },
-  SPADES: { name: "spades", char: "♠", n: 3, color: "black" },
+  h: { name: "hearts", char: "♥", n: 0, color: "red" },
+  d: { name: "diamonds", char: "♦", n: 1, color: "red" },
+  c: { name: "clubs", char: "♣", n: 2, color: "black" },
+  s: { name: "spades", char: "♠", n: 3, color: "black" },
 } as const;
 
-const SuitNtoSuit = {
-  h: "HEARTS",
-  d: "DIAMONDS",
-  c: "CLUBS",
-  s: "SPADES",
-} as const;
+// move cards:
+//   pile -> finish(n)
+//   pile -> board(n)
+//   finish(n) -> board(n)
+//   board(n) -> board(n)
+//   board(n) -> finish(n)
+// open new card
 
 const CardValueToName = {
+  0: "-",
   1: "A",
   2: "2",
   3: "3",
@@ -39,25 +41,34 @@ const CardNameToValue = Object.fromEntries(
 
 type Card = {
   suit: keyof typeof CardSuits;
+  // todo add color?
+  // todo add finish id ??
   value: keyof typeof CardValueToName;
   isFaceUp: boolean;
 };
 
 type CardStack = Card[]; // (Card deck)
+type Board = [
+  CardStack,
+  CardStack,
+  CardStack,
+  CardStack,
+  CardStack,
+  CardStack,
+  CardStack,
+];
+type Finish = [CardStack, CardStack, CardStack, CardStack];
 
-let pile: Card[] = [];
-
-let board: [
-  CardStack,
-  CardStack,
-  CardStack,
-  CardStack,
-  CardStack,
-  CardStack,
-  CardStack,
-] = [[], [], [], [], [], [], []];
-
-let finish: [CardStack, CardStack, CardStack, CardStack] = [[], [], [], []];
+let GAME = {
+  pile: [] as Card[],
+  board: [[], [], [], [], [], [], []] as Board,
+  finish: [
+    [{ suit: "h", value: 0, isFaceUp: false }],
+    [{ suit: "d", value: 0, isFaceUp: false }],
+    [{ suit: "c", value: 0, isFaceUp: false }],
+    [{ suit: "s", value: 0, isFaceUp: false }],
+  ] as Finish,
+};
 
 function generateShuffledCards() {
   const cards: CardStack = [];
@@ -81,31 +92,39 @@ function generateShuffledCards() {
   return cards;
 }
 
-function getCardColor(card: Card) {
-  return CardSuits[card.suit].color;
-}
-
-function canCardFollow(cardA: Card, cardB: Card) {
-  if (cardA.value + 1 === cardB.value) {
-    if (getCardColor(cardA) !== getCardColor(cardB)) {
-      return true;
-    }
+function canPutCard(cardA: Card, cardB: Card) {
+  if (
+    cardA.value + 1 === cardB.value &&
+    CardSuits[cardA.suit].color !== CardSuits[cardB.suit].color
+  ) {
+    return true;
   }
 
   return false;
 }
 
 function newGame(initCards?: string) {
+  GAME = {
+    pile: [] as Card[],
+    board: [[], [], [], [], [], [], []] as Board,
+    finish: [
+      [{ suit: "h", value: 0, isFaceUp: false }],
+      [{ suit: "d", value: 0, isFaceUp: false }],
+      [{ suit: "c", value: 0, isFaceUp: false }],
+      [{ suit: "s", value: 0, isFaceUp: false }],
+    ] as Finish,
+  };
+
   const cards = initCards
     ? splitStingBy2(initCards).map((p) => {
         let value = CardNameToValue[p[0]];
-        let suit = SuitNtoSuit[p[1] as "h" | "d" | "c" | "s"];
+        let suit = p[1] as "h" | "d" | "c" | "s";
 
         return { suit, value, isFaceUp: false };
       })
     : generateShuffledCards();
 
-  board = [[], [], [], [], [], [], []];
+  GAME.board = [[], [], [], [], [], [], []];
 
   for (let i = 0; i < 7; i++) {
     for (let j = 0; j <= i; j++) {
@@ -114,42 +133,52 @@ function newGame(initCards?: string) {
         if (j === i) {
           card.isFaceUp = true;
         }
-        board[i].push(card);
+        GAME.board[i].push(card);
       }
     }
   }
 
-  pile = cards;
+  GAME.finish = [
+    [{ suit: "h", value: 0, isFaceUp: false }],
+    [{ suit: "d", value: 0, isFaceUp: false }],
+    [{ suit: "c", value: 0, isFaceUp: false }],
+    [{ suit: "s", value: 0, isFaceUp: false }],
+  ];
+
+  GAME.pile = cards;
 }
 
 function printCard(card?: Card) {
-  if (!card) return `--`;
+  if (!card || card.value === 0) return `--`;
   return `${CardValueToName[card.value]}${CardSuits[card.suit].char}`;
 }
 
-function isNumber(n: any): n is number {
-  return typeof n === "number";
-}
-
-function move(from: "p" | number, to: "f" | number) {
+function move(
+  from: "f0" | "f1" | "f2" | "f3" | "p" | number,
+  to: "f" | number,
+) {
   if (from === "p" && to === "f") {
     putPileCardToFinish();
   } else if (from === "p" && isNumber(to)) {
     putPileCardToBoard(to);
+  } else if (["f0", "f1", "f2", "f3"].includes(from as any) && isNumber(to)) {
+    // @ts-ignore
+    let fromId = +from[1];
+    moveFinishCardToBoard(fromId, to);
   } else if (isNumber(from) && to === "f") {
     moveBoardCardToFinish(from);
   } else if (isNumber(from) && isNumber(to)) {
     moveBoardCard(from, to);
   } else {
-    throw new Error(`Inreachable move`);
+    throw new Error(`Inreachable code`);
   }
 }
 
 function moveBoardCard(pileA: number, pileB: number) {
-  let targetCard = board[pileB].at(-1);
+  let targetCard = GAME.board[pileB].at(-1);
   let pileACardVal = targetCard ? targetCard.value - 1 : 13;
 
-  let cardN = board[pileA].findLastIndex(
+  let cardN = GAME.board[pileA].findLastIndex(
     (c) => c.isFaceUp && c.value === pileACardVal,
   );
 
@@ -158,19 +187,19 @@ function moveBoardCard(pileA: number, pileB: number) {
     return false;
   }
 
-  let toMove = board[pileA].slice(cardN);
+  let toMove = GAME.board[pileA].slice(cardN);
   let topMoveCard = toMove[0];
+  let toPile = GAME.board[pileB];
 
-  let toPile = board[pileB];
   cl("Move card", printCard(topMoveCard), printCard(toPile.at(-1)));
 
   if (
     (toPile.length === 0 && topMoveCard.value === 13) ||
-    canCardFollow(topMoveCard, toPile.at(-1)!)
+    canPutCard(topMoveCard, toPile.at(-1)!)
   ) {
-    board[pileA] = board[pileA].slice(0, cardN);
-    if (board[pileA].at(-1)) {
-      board[pileA].at(-1)!.isFaceUp = true;
+    GAME.board[pileA] = GAME.board[pileA].slice(0, cardN);
+    if (GAME.board[pileA].at(-1)) {
+      GAME.board[pileA].at(-1)!.isFaceUp = true;
     }
     toPile.push(...toMove);
     return true;
@@ -180,164 +209,172 @@ function moveBoardCard(pileA: number, pileB: number) {
   return false;
 }
 
-function moveBoardCardToFinish(pile: number) {
-  let moveCard = board[pile].at(-1);
-
+function canMoveBoardCardToFinish(
+  pileId: number,
+): { card: Card } | { error: string } {
+  let moveCard = GAME.board[pileId].at(-1);
   if (!moveCard) {
-    cl("No card to move (no)", pile);
-    return false;
+    return { error: "No card to move" };
   }
 
-  let finishPile = finish[CardSuits[moveCard.suit].n];
-
-  if (finishPile.length === 0) {
-    if (moveCard.value === 1) {
-      board[pile].pop();
-      finishPile.push(moveCard);
-
-      let newLast = board[pile].at(-1);
-      if (newLast && newLast.isFaceUp === false) {
-        newLast.isFaceUp = true;
-      }
-
-      return true;
-    } else {
-      cl("Can not move (not Ace)", moveCard);
-      return false;
-    }
-  }
-
+  let finishPile = GAME.finish[CardSuits[moveCard.suit].n];
   let lastFinishCard = finishPile.at(-1)!;
 
   if (
     lastFinishCard.suit === moveCard.suit &&
-    lastFinishCard.value + 1 === moveCard.value
+    moveCard.value === lastFinishCard.value + 1
   ) {
-    board[pile].pop();
-    finishPile.push(moveCard);
-
-    let newLast = board[pile].at(-1);
-    if (newLast && newLast.isFaceUp === false) {
-      newLast.isFaceUp = true;
-    }
-
-    return true;
+    return { card: moveCard };
   }
 
-  cl("Can not move (no match)", printCard(moveCard), printCard(lastFinishCard));
+  return { error: "Can't move to finish" };
+}
+
+function moveBoardCardToFinish(pile: number) {
+  let can = canMoveBoardCardToFinish(pile);
+
+  if ("card" in can) {
+    GAME.board[pile].pop();
+    GAME.finish[CardSuits[can.card.suit].n].push(can.card);
+
+    let newLast = GAME.board[pile].at(-1);
+    if (newLast && !newLast.isFaceUp) {
+      newLast.isFaceUp = true;
+    }
+    return true;
+  } else {
+    cl("Can't move to finish", pile);
+  }
+}
+
+function moveFinishCardToBoard(finishId: number, pileId?: number) {
+  let finishPile = GAME.finish[finishId];
+  let finishCard = finishPile.at(-1);
+  if (!finishCard) {
+    cl("No card to move");
+    return false;
+  }
+
+  if (pileId === undefined) {
+    throw new Error(`TODO`);
+  } else {
+    let toPile = GAME.board[pileId];
+    if (
+      (toPile.length === 0 && finishCard.value === 13) ||
+      (toPile.length > 0 && canPutCard(finishCard, toPile.at(-1)!))
+    ) {
+      finishPile.pop();
+      toPile.push(finishCard);
+      return true;
+    }
+  }
+
+  cl(`'Can not move finish card ${finishId} to board'`);
   return false;
 }
 
 function openPileCard() {
-  if (pile.length === 0) {
+  if (GAME.pile.length === 0) {
     cl("Empty pile");
     return false;
   }
 
   let lastClose = -1;
-  for (let i = pile.length - 1; i >= 0; i--) {
-    if (!pile[i].isFaceUp) {
+  for (let i = GAME.pile.length - 1; i >= 0; i--) {
+    if (!GAME.pile[i].isFaceUp) {
       lastClose = i;
       break;
     }
   }
 
-  if (lastClose > -1) {
-    pile[lastClose].isFaceUp = true;
-    return;
+  if (lastClose === -1) {
+    GAME.pile.forEach((card) => (card.isFaceUp = false));
+  } else {
+    GAME.pile[lastClose].isFaceUp = true;
+  }
+}
+
+function getTopOpenPileCard() {
+  return GAME.pile.find((c) => c.isFaceUp);
+}
+
+function canPutPileCardToBoard(
+  toPile: number,
+): { card: Card } | { error: string } {
+  let topOpenPileCard = getTopOpenPileCard();
+  if (!topOpenPileCard) {
+    return { error: "No open pile card" };
   }
 
-  pile.forEach((card) => {
-    card.isFaceUp = false;
-  });
-  pile.at(-1)!.isFaceUp = true;
+  let lastToPileCard = GAME.board[toPile].at(-1);
+
+  if (
+    (!lastToPileCard && topOpenPileCard.value === 13) ||
+    (lastToPileCard && canPutCard(topOpenPileCard, lastToPileCard))
+  ) {
+    return { card: topOpenPileCard };
+  }
+
+  return {
+    error: `Can not put ${printCard(topOpenPileCard)} to ${printCard(lastToPileCard!)}`,
+  };
 }
 
 function putPileCardToBoard(toPile: number) {
-  let topOpenPileCardId = pile.findIndex((c) => c.isFaceUp);
+  let can = canPutPileCardToBoard(toPile);
 
-  if (topOpenPileCardId === -1) {
-    cl("No open pile card");
-    return false;
-  }
-
-  let topOpenPileCard = pile[topOpenPileCardId];
-
-  let toPileCards = board[toPile];
-
-  if (toPileCards.length === 0) {
-    if (topOpenPileCard.value === 13) {
-      pile.splice(topOpenPileCardId, 1);
-      toPileCards.push(topOpenPileCard);
-      return true;
-    }
-
-    cl(`Can not put ${printCard(topOpenPileCard)} to empty pile`);
-    return false;
-  }
-
-  let lastToPileCard = toPileCards.at(-1);
-
-  if (canCardFollow(topOpenPileCard, lastToPileCard!)) {
-    pile.splice(topOpenPileCardId, 1);
-    toPileCards.push(topOpenPileCard);
+  if ("card" in can) {
+    removeItem(GAME.pile, can.card);
+    GAME.board[toPile].push(can.card);
     return true;
+  } else {
+    cl(can.error);
+  }
+}
+
+function canPutPileCardToFinish(
+  finishId: number,
+): { card: Card } | { error: string } {
+  let moveCard = getTopOpenPileCard();
+  if (!moveCard) {
+    return { error: "No open pile card" };
   }
 
-  cl(
-    `Can not put ${printCard(topOpenPileCard)} to ${printCard(lastToPileCard!)}`,
-  );
+  let finishPileCard = GAME.finish[finishId].at(-1)!;
+  if (
+    finishPileCard.suit !== moveCard.suit ||
+    moveCard.value != finishPileCard.value + 1
+  ) {
+    return { error: `Can not put ${printCard(moveCard)} to the finish` };
+  }
+
+  return { card: moveCard };
 }
 
 function putPileCardToFinish() {
-  let topOpenPileCardId = pile.findIndex((c) => c.isFaceUp);
+  let finishIds = [0, 1, 2, 3];
 
-  if (topOpenPileCardId === -1) {
-    cl("No open pile card");
-    return false;
-  }
-
-  let topOpenPileCard = pile[topOpenPileCardId];
-
-  let finishPile = finish[CardSuits[topOpenPileCard.suit].n];
-
-  if (finishPile.length === 0) {
-    if (topOpenPileCard.value === 1) {
-      pile.splice(topOpenPileCardId, 1);
-      finishPile.push(topOpenPileCard);
+  for (let id of finishIds) {
+    let can = canPutPileCardToFinish(id);
+    if ("card" in can) {
+      removeItem(GAME.pile, can.card);
+      GAME.finish[id].push(can.card);
       return true;
     }
-
-    cl(`Can not put ${printCard(topOpenPileCard)} to empty pile`);
-    return false;
   }
 
-  let lastFinishCard = finishPile.at(-1);
-
-  if (
-    lastFinishCard &&
-    lastFinishCard.suit === topOpenPileCard.suit &&
-    lastFinishCard.value + 1 === topOpenPileCard.value
-  ) {
-    pile.splice(topOpenPileCardId, 1);
-    finishPile.push(topOpenPileCard);
-    return true;
-  }
-
-  cl(
-    `Can not put ${printCard(topOpenPileCard)} to ${printCard(lastFinishCard)}`,
-  );
+  cl(`Can not put pile to the finish`);
 }
 
 function renderGame() {
-  let closedPile = pile.filter((c) => !c.isFaceUp).length;
-  let openPile = pile.length - closedPile;
-  let lastOpenCard = pile.find((c) => c.isFaceUp);
+  let closedPile = GAME.pile.filter((c) => !c.isFaceUp).length;
+  let openPile = GAME.pile.length - closedPile;
+  let lastOpenCard = getTopOpenPileCard();
 
-  let f0 = printCard(finish[0].at(-1));
-  let f1 = printCard(finish[1].at(-1));
-  let f2 = printCard(finish[2].at(-1));
-  let f3 = printCard(finish[3].at(-1));
+  let f0 = printCard(GAME.finish[0].at(-1));
+  let f1 = printCard(GAME.finish[1].at(-1));
+  let f2 = printCard(GAME.finish[2].at(-1));
+  let f3 = printCard(GAME.finish[3].at(-1));
 
   cl(
     closedPile,
@@ -349,26 +386,62 @@ function renderGame() {
   renderBoard();
 
   cl("");
-}
 
-function renderBoard() {
-  cl(" 0  1  2  3  4  5  6");
+  function renderBoard() {
+    cl(" 0  1  2  3  4  5  6");
 
-  for (let i = 0; i < 100; i++) {
-    let row = "";
+    for (let i = 0; i < 100; i++) {
+      let row = "";
 
-    for (let j = 0; j < 7; j++) {
-      let card = board[j][i];
-      if (card) {
-        row += card.isFaceUp ? `${printCard(card)} ` : "XX ";
-      } else {
-        row += "   ";
+      for (let j = 0; j < 7; j++) {
+        let card = GAME.board[j][i];
+        if (card) {
+          row += card.isFaceUp ? `${printCard(card)} ` : "XX ";
+        } else {
+          row += "   ";
+        }
       }
+      if (row.trim() === "") break;
+      cl(row);
     }
-    if (row.trim() === "") break;
-    cl(row);
   }
 }
+
+// =============================
+
+function cl(...args: any) {
+  console.log(...args);
+}
+
+function keys<O extends {}>(obj: O) {
+  return Object.keys(obj) as (keyof O)[];
+}
+
+function randomWithSeed(seed: number) {
+  let value = seed;
+
+  return function rand() {
+    value = (value * 16807) % 2147483647;
+    return value;
+  };
+}
+
+function splitStingBy2(str: string) {
+  return str.match(/.{1,2}/g)!;
+}
+
+function isNumber(n: any): n is number {
+  return typeof n === "number";
+}
+
+function removeItem<T>(arr: T[], it: T): void {
+  let i = arr.indexOf(it);
+  if (i !== -1) {
+    arr.splice(i, 1);
+  }
+}
+
+// ===========================
 
 newGame(
   "Js3d0cQs2sQhKs8s0hQc4d4s5c7h3sJc9sKc7c5s8d6d2c2d6sKh3c5d8c6hQd2hKdJh8hJd9h9cAs3hAc5h0d7dAh4h4c0sAd6c7s9d",
@@ -430,6 +503,7 @@ move(1, 5);
 move("p", 1);
 openPileCard();
 openPileCard();
+openPileCard();
 move("p", 5);
 openPileCard();
 openPileCard();
@@ -443,6 +517,7 @@ openPileCard();
 move("p", "f");
 openPileCard();
 move("p", 5);
+openPileCard();
 openPileCard();
 openPileCard();
 openPileCard();
@@ -469,26 +544,3 @@ openPileCard();
 openPileCard();
 
 renderGame();
-
-// =============================
-
-function cl(...args: any) {
-  console.log(...args);
-}
-
-function keys<O extends {}>(obj: O) {
-  return Object.keys(obj) as (keyof O)[];
-}
-
-function randomWithSeed(seed: number) {
-  let value = seed;
-
-  return function rand() {
-    value = (value * 16807) % 2147483647;
-    return value;
-  };
-}
-
-function splitStingBy2(str: string) {
-  return str.match(/.{1,2}/g)!;
-}
