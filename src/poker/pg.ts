719aa7f8fcbb8b521244e88b8a0c3ca696f316ea
Joxter <joxterrr@gmail.com>
2024-03-23 11:29:21 +0100

    try poker

--- src/poker/pg.ts
+++ src/poker/pg.ts
@@ -13,41 +13,25 @@ play();
 function play() {
   if (!table.currentActor) return;
 
-  // player 1 (dealer) is first to act.
-  // Player 2 and 3 posted blinds.
+  table.currentActor.raiseAction(20);
   table.currentActor.callAction();
-
-  // player 2 is first to act on the flop.
+  table.currentActor.callAction();
+  // table.currentActor.callAction();
+  console.log(table.communityCards.map((c) => c.rank + c.suitChar));
+  table.currentActor.betAction(30);
+  table.currentActor.callAction();
+  table.currentActor.callAction();
+  console.log(table.communityCards.map((c) => c.rank + c.suitChar));
+  table.currentActor.betAction(50);
+  table.currentActor.callAction();
+  table.currentActor.callAction();
   table.currentActor.checkAction();
-
-  // player 3 decides to open the bet on the flop.
-  table.currentActor.betAction(20);
-
-  // player 1 raises.
-  table.currentActor.raise(40);
-
-  // player 2 calls.
-  table.currentActor.call();
-
-  // player 3 calls player 1's raise.
-  table.currentActor.call();
-
-  // betting has been met, player 2 is first to act on the turn and all three decide to check.
   table.currentActor.checkAction();
   table.currentActor.checkAction();
   table.currentActor.checkAction();
 
-  // player 2 is first to act on the river and decides
-  // to open the bet at $40.
-  table.currentActor.betAction(40);
+  console.log(table.communityCards.map((c) => c.rank + c.suitChar));
 
-  // player 3 raises to $60.
-  table.currentActor.raiseAction(60);
-
-  // player 1 folds.
-  table.currentActor.foldAction();
-
-  // Declare winner(s)!
-
+  console.log(table.currentActor.legalActions(), table.currentActor.id);
   console.log(table.winners);
 }
