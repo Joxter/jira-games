c3fb1eb92e59d14bdbb8e0acf29ac66e425d8fb2
Joxter <joxterrr@gmail.com>
2025-01-18 15:37:47 +0100

    add generateFromSchema (wip)

---
+++ src/sudoku/puzzle-utils.ts
@@ -0,0 +1,75 @@
+type Index = number;
+
+// init: string[];
+// answer: string[];
+
+type MyPuzzle = {
+  power: number;
+  //
+  rows: Array<Index[]>;
+  cols: Array<Index[]>;
+  boxes: Array<Index[]>;
+  //
+  width: number;
+  height: number;
+  //
+  // borders?: Array<[number, number]>; // todo add borders
+};
+
+export function generateFromSchema(schema: string): MyPuzzle {
+  schema = schema.trim();
+  let grid = schema.split("\n").map((row, i) => {
+    return row.split("");
+  });
+  let chars = [...new Set(schema.replace(/\s/g, "").split(""))];
+
+  let boxes = Object.fromEntries(chars.map((ch) => [ch, [] as number[]]));
+  let rows = Object.fromEntries(chars.map((ch) => [ch, [] as number[]]));
+  let cols = Object.fromEntries(chars.map((ch) => [ch, [] as number[]]));
+
+  for (let i = 0; i < grid.length; i++) {
+    let rowCh = chars[i];
+    for (let j = 0; j < grid[i].length; j++) {
+      let colCh = chars[j];
+
+      let ch = grid[i][j];
+      let index = i * grid.length + j;
+      boxes[ch].push(index);
+      rows[rowCh].push(index);
+      cols[colCh].push(index);
+    }
+  }
+
+  let borders = [];
+  let borderW = 2;
+  let borderH = 10;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let ch = grid[i][j];
+      let leftCh = grid[i][j - 1];
+      let topCh = grid[i - 1]?.[j];
+
+      if (ch !== leftCh) {
+        //
+      }
+      if (ch !== topCh) {
+        //
+      }
+    }
+  }
+
+  return {
+    power: chars.length,
+    rows: chars.map((ch) => rows[ch]),
+    cols: chars.map((ch) => cols[ch]),
+    boxes: chars.map((ch) => boxes[ch]),
+    width: grid.length,
+    height: grid[0].length,
+  };
+}
+
+// todo
+//  - getRowByIndex
+//  - getColByIndex
+//  - getBoxByIndex
