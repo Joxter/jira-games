c9248e106b269f1c077c29a1b2fd4cab7183fb0e
Joxter <joxterrr@gmail.com>
2025-01-22 22:26:23 +0100

    improve generateFromSchema

--- src/sudoku/puzzle-utils.ts
+++ src/sudoku/puzzle-utils.ts
@@ -12,8 +12,9 @@ type MyPuzzle = {
   cols: Array<Index[]>;
   boxes: Array<Index[]>;
   //
-  width: number;
-  height: number;
+  rowByIndex: Array<Index[]>;
+  colByIndex: Array<Index[]>;
+  boxByIndex: Array<Index[]>;
   //
   getBorders: (
     borderW: number,
@@ -38,33 +39,34 @@ export function generateFromSchema(schema: string): MyPuzzle {
   });
 
   let chars: string[] = [];
-  let originalCoordinates = [];
+  let originalCoordinates: Array<[number, number]> = [];
   grid.forEach((row, rowI) => {
     row.forEach((ch, colI) => {
       if (!chars.includes(ch)) chars.push(ch);
       originalCoordinates.push([rowI, colI]);
     });
   });
+  console.log(originalCoordinates);
 
   let boxes = Object.fromEntries(chars.map((ch) => [ch, [] as number[]]));
-  let rows = Object.fromEntries(chars.map((ch) => [ch, [] as number[]]));
-  let cols = Object.fromEntries(chars.map((ch) => [ch, [] as number[]]));
+  let rows = chars.map(() => [] as number[]);
+  let cols = chars.map(() => [] as number[]);
 
   for (let i = 0; i < grid.length; i++) {
-    let rowCh = chars[i];
     for (let j = 0; j < grid[i].length; j++) {
-      let colCh = chars[j];
-
       let ch = grid[i][j];
       let index = i * grid.length + j;
+
       boxes[ch].push(index);
-      rows[rowCh].push(index);
-      cols[colCh].push(index);
+      rows[i].push(index);
+      cols[j].push(index);
     }
   }
 
-  function getBorders(borderW: number, borderH: number) {
+  function getBorders(borderW: number, borderLen: number) {
     let borders = [];
+    let borderSize = borderW + borderW + borderLen;
+    let k = borderW + borderLen;
 
     for (let i = 0; i < grid.length + 1; i++) {
       for (let j = 0; j < grid[0].length + 1; j++) {
@@ -72,17 +74,13 @@ export function generateFromSchema(schema: string): MyPuzzle {
         let leftCh = grid[i]?.[j - 1];
         let topCh = grid[i - 1]?.[j];
 
-        let borderSize = borderW + borderW + borderH;
-        let leftTop = {
-          left: j * borderSize - j * borderW,
-          top: i * borderSize - i * borderW,
-        };
+        let leftTop = { left: j * k, top: i * k };
 
         if (ch !== leftCh) {
-          borders.push({ ...leftTop, width: 2, height: borderSize });
+          borders.push({ ...leftTop, width: borderW, height: borderSize });
         }
         if (ch !== topCh) {
-          borders.push({ ...leftTop, width: borderSize, height: 2 });
+          borders.push({ ...leftTop, width: borderSize, height: borderW });
         }
       }
     }
@@ -90,18 +88,16 @@ export function generateFromSchema(schema: string): MyPuzzle {
     return borders;
   }
 
+  console.log(boxes);
+
   return {
     getBorders,
     power: chars.length,
-    rows: chars.map((ch) => rows[ch]),
-    cols: chars.map((ch) => cols[ch]),
+    rows: rows,
+    cols: cols,
     boxes: chars.map((ch) => boxes[ch]),
-    width: grid.length,
-    height: grid[0].length,
+    rowByIndex: originalCoordinates.map(([row, col]) => rows[row]),
+    colByIndex: originalCoordinates.map(([row, col]) => cols[col]),
+    boxByIndex: originalCoordinates.map(([row, col]) => boxes[grid[row][col]]),
   };
 }
-
-// todo
-//  - getRowByIndex
-//  - getColByIndex
-//  - getBoxByIndex
